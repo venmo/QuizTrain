65b463f99304af2287c26d6d01e339b76fbc105e
Dave Gallagher <david.gallagher@venmo.com>
2018-06-14 10:27:36 -0700

    Implemented private(set) on properties which need private setters.

--- QuizTrain/Network/Operations/GetConfigurationGroupsOperation.swift
+++ QuizTrain/Network/Operations/GetConfigurationGroupsOperation.swift
@@ -4,14 +4,13 @@ class GetConfigurationGroupsOperation: AsyncOperation {
 
     // MARK: Properties
 
-    private weak var _api: ObjectAPI?
-    var api: ObjectAPI? { return _api }
+    private(set) weak var api: ObjectAPI?
     let projectId: Project.Id
 
     // MARK: Init
 
     init(api: ObjectAPI, projectId: Project.Id) {
-        self._api = api
+        self.api = api
         self.projectId = projectId
     }
 
@@ -33,14 +32,13 @@ class GetConfigurationGroupsOperation: AsyncOperation {
         state = .executing
 
         api.getConfigurationGroups(inProjectWithId: projectId) { [weak self] (outcome) in
-            self?._outcome = outcome
+            self?.outcome = outcome
             self?.state = .finished
         }
     }
 
     // MARK: Completion
 
-    private var _outcome: Outcome<[ConfigurationGroup], ObjectAPI.GetError>?
-    var outcome: Outcome<[ConfigurationGroup], ObjectAPI.GetError>? { return _outcome }
+    private(set) var outcome: Outcome<[ConfigurationGroup], ObjectAPI.GetError>?
 
 }
--- QuizTrain/Network/Operations/GetProjectOperation.swift
+++ QuizTrain/Network/Operations/GetProjectOperation.swift
@@ -4,14 +4,13 @@ class GetProjectOperation: AsyncOperation {
 
     // MARK: Properties
 
-    private weak var _api: ObjectAPI?
-    var api: ObjectAPI? { return _api }
+    private(set) weak var api: ObjectAPI?
     let projectId: Project.Id
 
     // MARK: Init
 
     init(api: ObjectAPI, projectId: Project.Id) {
-        self._api = api
+        self.api = api
         self.projectId = projectId
     }
 
@@ -33,14 +32,13 @@ class GetProjectOperation: AsyncOperation {
         state = .executing
 
         api.getProject(projectId) { [weak self] (outcome) in
-            self?._outcome = outcome
+            self?.outcome = outcome
             self?.state = .finished
         }
     }
 
     // MARK: Completion
 
-    private var _outcome: Outcome<Project, ObjectAPI.GetError>?
-    var outcome: Outcome<Project, ObjectAPI.GetError>? { return _outcome }
+    private(set) var outcome: Outcome<Project, ObjectAPI.GetError>?
 
 }
