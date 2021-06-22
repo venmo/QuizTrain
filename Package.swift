906279550e71d4ba9badc012b6485e226dc4b22a
Justin <daemedeor@users.noreply.github.com>
2021-06-22 11:04:34 -0700

    Update Package.swift
    Co-authored-by: Mark Adams <hyperspacemark@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "QuizTrain",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "QuizTrain",
             targets: ["QuizTrain"]),
